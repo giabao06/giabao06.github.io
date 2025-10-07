9cee159c6b54964a3afee8b2331030286cca553b
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2025-10-07 15:40:18 +0700

    add post dropdown

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,7 +1,14 @@
+"use client"
+
 import Image from 'next/image';
 import Link from "next/link";
+import {useEffect} from "react";
+import {themeChange} from "theme-change";
 
 export default function Navbar() {
+    useEffect(() => {
+        themeChange(false)
+    }, [])
     return(
         <div className={"navbar bg-base-100 shadow-lg"}>
             <div className={"navbar-start"}>
@@ -20,10 +27,19 @@ export default function Navbar() {
                 <ul className={"menu menu-horizontal px-1 lg:flex space-x-10"}>
                     <li className={"nav-item"}><Link href={"/about"}>About Me</Link></li>
                     <li className={"nav-item"}><Link href={"/projects"}>Current Projects</Link></li>
+                    <li className={"nav-item"}>
+                        <details className={"dropdown"}>
+                            <summary>Stuff/Posts</summary>
+                            <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                                {/* TODO: Dynamically load posts */}
+                                <li><a>These are</a></li>
+                                <li><a>Some Posts</a></li>
+                            </ul>
+                        </details>
+                    </li>
                 </ul>
             </div>
             <div className={"navbar-end"}>
-
             </div>
         </div>
     )
